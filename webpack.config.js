8a41ed9c921287868808646d6924abcdfac42bbd
Francesco Paolo Vitullo <FVitullo@sk.ibm.com>
2017-05-26 09:31:53 +0200

    added babel and changed package.json

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
   },
   module: {
     rules: [
+        {
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: [{
+                loader: 'babel-loader',
+                options: { presets: ['es2015'] },
+            }],
+        },
         { test: /\.wat$/, loader: "wast-loader", exclude: /node_modules/ }
     ]
   },
