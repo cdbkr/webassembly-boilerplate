885fbe75cb42ac6440a86565dc9c8872586211ca
Francesco Paolo Vitullo <FVitullo@sk.ibm.com>
2017-06-11 22:40:50 +0200

    added debug instead of console.log

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import add from './add.wat';
 import fact from './fact.wat';
 import now from 'performance-now';
+import debug from 'debug';
+
+const infoWasm = debug('WASM');
+const infoNative = debug('Native');
 
 WebAssembly.instantiate(fact)
 .then(module => {
-    console.log(`Starting WASM execution`);
-    const startWasm = now();
-    let factorial = module.instance.exports.fact(10);
-    console.log(`WASM execution finished in: ${(now() - startWasm).toFixed(10)}ms with factorial ${factorial}`);
+        infoWasm(`Starting WASM execution`);
+        const startWasm = now();
+        let factorial = module.instance.exports.fact(10);
+        infoWasm(`WASM execution finished in: ${(now() - startWasm).toFixed(10)}ms with factorial ${factorial}`);
     });
 
 
-let factorial = function (n) {
+let factorial = (n) => {
     if (n === 0) {
         return 1;
     }
     return n * factorial(n - 1);
 };
-console.log(`Starting Native execution`);
+infoNative(`Starting Native execution`);
 const startNative = now();
 const nativeFactorial = factorial(10);
-console.log(`Native execution finished in: ${(now() - startNative).toFixed(10)}ms with factorial ${nativeFactorial}`);
+infoNative(`Native execution finished in: ${(now() - startNative).toFixed(10)}ms with factorial ${nativeFactorial}`);
 
